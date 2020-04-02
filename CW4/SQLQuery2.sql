9b228c74d4d6ea45cfd7d96ce0dac32f233cc50e
Oleksandr Rudenko <s19434@pjwstk.edu.pl>
2020-04-02 03:37:21 +0200

    FINAL

---
+++ CW4/SQLQuery2.sql
@@ -0,0 +1,9 @@
+﻿use s19434
+insert student(IndexNumber,FirstName,LastName,BirthDate,IdEnrollment)
+values('s19434','dkwd','fjeajf','2008-11-11',1)
+
+insert student(IndexNumber,FirstName,LastName,BirthDate,IdEnrollment)
+values('s19435','wdawd','awdwad','2009-11-11',2)
+
+insert student(IndexNumber,FirstName,LastName,BirthDate,IdEnrollment)
+values('s19436','wad','efewfe','2010-11-11',3)
